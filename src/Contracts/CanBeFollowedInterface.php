481c978ff10096f4553743b158b557b67ca8a341
Alexander Manfred Pöllmann <alex@vendocr.at>
2017-07-26 16:48:32 +0200

    Rename/refactor interfaces

--- src/Contracts/CanBeFollowedInterface.php
+++ src/Contracts/CanBeFollowedInterface.php
@@ -1,6 +1,6 @@
 <?php namespace Lecturize\Followers\Contracts;
 
-interface CanFollowInterface
+interface CanBeFollowedInterface
 {
     //
 }
--- src/Contracts/FollowableInterface.php
+++
@@ -1,9 +0,0 @@
-<?php namespace Lecturize\Followers\Contracts;
-
-interface FollowableInterface
-{
-    /**
-     * @return mixed
-     */
-//	public function followers();
-}
